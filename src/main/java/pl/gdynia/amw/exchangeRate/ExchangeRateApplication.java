898bd70240d5ee54083bdd5ff8999a45bd843422
Michał Gryczewski <72730947+michalGryczewski1998@users.noreply.github.com>
2022-01-24 21:29:24 +0100

    Dodanie obsługi bazy danych MySQL

--- src/main/java/pl/gdynia/amw/exchangeRate/ExchangeRateApplication.java
+++ src/main/java/pl/gdynia/amw/exchangeRate/ExchangeRateApplication.java
@@ -3,8 +3,6 @@ package pl.gdynia.amw.exchangeRate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import pl.gdynia.amw.exchangeRate.download.Download;
-import pl.gdynia.amw.exchangeRate.model.ExchangeRateRepository;
 
 @SpringBootApplication
 public class ExchangeRateApplication {
